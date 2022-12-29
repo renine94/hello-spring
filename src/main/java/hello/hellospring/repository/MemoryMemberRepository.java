6cd365d3a76bde0a94b92f9d503ff654593b9a26
kyle <jg.kang@fitpet.co.kr>
2022-12-30 02:43:22 +0900

    [자바코드로 지접 스프링 빈 등록하기] SpringConfig 클래스에 직접 Bean 을 등록해서 만들어준다. (나중에 구현체 바뀌는것들은 직접 빈등록하면 좋음)

--- src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
+++ src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
@@ -5,7 +5,6 @@ import org.springframework.stereotype.Repository;
 
 import java.util.*;
 
-@Repository
 public class MemoryMemberRepository implements MemberRepository{
 
     // 동시성 문제를 고려하게되면, 실무에서는 ConcurrencyHashMap 을 써야하지만 예제이므로 그냥 HashMap 자료구조 사용
